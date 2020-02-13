f963ec8aebcafad5ec5e5f33233512cc60e06013
Luis Reyes <luisreyessegner@gmail.com>
2020-02-12 22:35:05 -0300

    Últimos cambios Luis

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,49 +1,41 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			users: [
-				{
-					id: 1,
-					fullname: "Luis Reyes",
-					email: "luis@gmail.com",
-					password: "123456"
-				}
-			],
-			meetings: [
-				{
-					id: 1,
-					user_id: 1,
-					admin: "Luis Reyes",
-					create_date: "10-02-2020",
-					meeting_hour: "16:00 pm",
-					project_name: "tests front-back",
-					title: "test 1",
-					place: "Sala 4geeks",
-					description: "2 weeks to go",
-					target: "terminar final project"
-				}
-			],
-			topics: [
-				{
-					id: 1,
-					meeting_id: 1,
-					title: "title 1",
-					priority: "Alta",
-					notes: "notes 1",
-					care: "care 1",
-					tracking: "tracking",
-					duration: 10
-				}
-			],
-			guests: [
-				{
-					id: 1,
-					meeting_id: 1,
-					fullname: "Camilo Sánchez",
-					email: "camilo@gmail.com",
-					rol: "espectador"
-				}
-			]
+			current_user_id: 1,
+			meetings: [{
+				create_date: "4-02-2020",
+				description: "descripcion4",
+				guests: [],
+				meeting_date: "4-01-2020",
+				meeting_hour: "4:00 pm",
+				place: "place4",
+				project_name: "proyecto4",
+				target: "target4",
+				title: "title4",
+				topics: [
+					{
+						id: 1,
+						care: "care77",
+						duration: "duration5",
+						notes: "notes5",
+						priority: "priority5",
+						title: "title5",
+						tracking: "tracking5",
+						meeting_id: 1
+					},
+					{
+						id: 2,
+						care: "care88",
+						duration: "duration6",
+						notes: "notes6",
+						priority: "priority6",
+						title: "title6",
+						tracking: "tracking6",
+						meeting_id: 1
+					}
+				],
+				user_id: 1
+			}]
 		},
 		actions: {
 			// // Use getActions to call a function within a fuction
